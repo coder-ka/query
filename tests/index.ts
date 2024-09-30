3b0a529078e6b80467c8987ded4ec3b0d99cfbfe
Katsuyuki Oeda <katsu.amagramer@gmail.com>
2024-09-30 17:50:02 +0900

    fix test.

--- tests/index.ts
+++ tests/index.ts
@@ -2,10 +2,9 @@ import mysqlTest from "./mysql.test";
 import postgresTest from "./postgres.test";
 import queryTest from "./query.test";
 
-Promise.all([...queryTest, ...mysqlTest, ...postgresTest]).then(
-  (testResults) => {
-    testResults.forEach((testResult) => {
-      if (testResult.description) console.log(testResult.description);
-    });
-  }
+Promise.all(
+  [...queryTest, ...mysqlTest, ...postgresTest].map(async (test) => {
+    if (test.description) console.log(test.description);
+    await test.content();
+  })
 );
